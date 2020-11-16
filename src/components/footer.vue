177fcf97e94690380740c8550c7e3bc3c672ea1a
Leo <voonlihhaur@gmail.com>
2020-11-16 17:34:44 +0800

    Fix footer style

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        Made with <a href="https://cli.vuejs.org/">Vue CLI</a> by Leo Voon | Deploy with <a href="https://www.netlify.com/">Netlify</a>
+        Made with <a href="https://cli.vuejs.org/">Vue CLI</a> by Leo Voon <br>Deploy with <a href="https://www.netlify.com/">Netlify</a>
     </footer>
 </template>
 
@@ -8,12 +8,10 @@
 
 <style lang="scss" scoped>
     footer {
-        position: absolute;
-        bottom: 0;
         margin-top: 4em;
         padding: 1rem;
         background-color: gainsboro;
-
+        text-align: center;
         a {
             text-decoration: none;
             color: mediumaquamarine;
